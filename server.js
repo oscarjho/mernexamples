56eab29a36dd2c10ea42ceed97f36da9208c186c
Oscar <oscarjavierho@gmail.com>
2020-05-13 13:55:59 -0430

    bug fix

--- server.js
+++ server.js
@@ -22,14 +22,15 @@ mongoose
 app.use('/api/Items', items);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV == 'production') {
-  //Set static folder
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
-  
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 
 // Config Port
 const port = process.env.PORT || 5000;
