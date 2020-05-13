186252725b3e4b3b0e801ada0e885c29f79f82d9
Oscar <oscarjavierho@gmail.com>
2020-05-13 14:56:37 -0430

    bug fix

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use('/api/Items', items);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  //old -> app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
